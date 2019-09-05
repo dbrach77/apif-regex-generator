a9d9d89bf6c4dbd43c6d93a24a986c72e10e016d
dbrach77 <dbrach77@gmail.com>
2019-09-05 11:39:38 +0200

    some little refactoring

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 class Utils:
     def regexMap(self, s):
-        ishc = False
-        storefilter = False
-        store = False
-        isFilter = False
-        filter =''
+        isHardCode = False
+        endOfString = False
+        filter = ''
         hc = ''
         currentFilter = ''
 
@@ -15,62 +13,57 @@ class Utils:
         for n in s:
             l = l + 1
             isFilter = False
-            storefilter = False
-            if 'a' <= n and n <='z':
+            savefilter = False
+            if 'a' <= n <= 'z':
                 currentFilter = '[a-z]'
                 isFilter = True
-            elif 'A' <= n and n <='Z':
+            elif 'A' <= n <= 'Z':
                 currentFilter = '[A-Z]'
                 isFilter = True
-            elif '0' <= n and n <='9':
+            elif '0' <= n <= '9':
                 currentFilter = '[0-9]'
                 isFilter = True
             else:
                 hc = hc + n
-                ishc = True
+                isHardCode = True
 
-            if ishc == False:
+            if not isHardCode:
                 if filter == '':
                     filter = currentFilter
-                #se trovo un filtro ed è lo stesso di prima allora incremento il contatore
+                # se trovo un filtro ed è lo stesso di prima allora incremento il contatore
                 if filter == currentFilter:
                     rep = rep + 1
 
-            if ishc == True and isFilter == True:
-                storefilter = True
-
-            #se sono alla fine devo storare
+            # se sono alla fine devo storare
             if l == len(s):
-                store = True
-            #se ho un filtro e il nuovo è diverso salvo il filtro
-            if filter != currentFilter: #and l == len(s):
-                storefilter = True
+                endOfString = True
+            # se ho un filtro e il nuovo è diverso salvo il filtro
+            if filter != currentFilter or isHardCode == True and isFilter == True:
+                savefilter = True
 
-            if storefilter == True:
-                f = {'filter':filter,'repetitions':rep,'hc':hc}
-                filters.append(f)
+            if savefilter == True:
+                self.saveFilter(filter, filters, hc, rep)
                 filter = currentFilter
                 rep = 1
                 hc = ''
-                ishc = False
+                isHardCode = False
 
-            if store == True:
-                f = {'filter':currentFilter,'repetitions':rep,'hc':hc}
-                filters.append(f)
+            if endOfString == True:
+                self.saveFilter(currentFilter, filters, hc, rep)
                 filter = ''
-
-
 
         return filters
 
-    def regexString(self,m):
+    def saveFilter(self, filter, filters, hc, rep):
+        f = {'filter': filter, 'repetitions': rep, 'hc': hc}
+        filters.append(f)
+
+    def regexString(self, m):
         regex = ''
         for f in m:
-                if f['repetitions'] > 0:
-                    regex = regex + f['filter']+'{'+str(f['repetitions'])+'}'+f['hc']
-                else:
-                    regex = regex + f['filter']+ f['hc']
+            if f['repetitions'] > 0:
+                regex = regex + f['filter'] + '{' + str(f['repetitions']) + '}' + f['hc']
+            else:
+                regex = regex + f['filter'] + f['hc']
 
         return regex
-
-
